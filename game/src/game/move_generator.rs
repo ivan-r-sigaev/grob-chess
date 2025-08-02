d9817f4e5832c0b25070b18ff7e1a4ce930cb344
Ivan <98910154+ivan-r-sigaev@users.noreply.github.com>
2025-08-02 18:57:37 +0300

    Make PackedChessMove private
    Made PackedChessMove private.

--- game/src/game/move_generator.rs
+++ game/src/game/move_generator.rs
@@ -1,14 +1,14 @@
 use position::prelude::{ChessMove, ChessMoveHint, Position, Square};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct PackedChessMove {
+struct PackedChessMove {
     data: u16,
 }
 
 impl PackedChessMove {
     #[inline(always)]
     #[must_use]
-    pub fn new(chess_move: ChessMove) -> Self {
+    fn new(chess_move: ChessMove) -> Self {
         Self {
             data: (((chess_move.hint as u16) & 0xf) << 12)
                 | (((chess_move.from as u16) & 0x3f) << 6)
@@ -17,7 +17,7 @@ impl PackedChessMove {
     }
     #[inline(always)]
     #[must_use]
-    pub fn get(self) -> ChessMove {
+    fn get(self) -> ChessMove {
         let to = Square::from_repr((self.data & 0x3f) as u8).unwrap();
         let from = Square::from_repr(((self.data >> 6) & 0x3f) as u8).unwrap();
         let hint = ChessMoveHint::from_repr(((self.data >> 12) & 0x0f) as u8).unwrap();
