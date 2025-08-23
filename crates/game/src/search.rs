0f42dfc7cf78d43a73a94f7f5a731bfb71e2ed4e
Ivan <98910154+ivan-r-sigaev@users.noreply.github.com>
2025-08-23 17:20:24 +0300

    Add docs to ParallelSearch
    Added doc comments to game/src/search.rs.

--- crates/game/src/search.rs
+++ crates/game/src/search.rs
@@ -17,6 +17,7 @@ use position::ChessMove;
 
 use crate::{Game, GameEnding, GameSearch, Transposition, TranspositionTable, Waiter};
 
+/// How advantageous is a chess position.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Score {
     /// Length of a forced mate sequence in turns (good).
@@ -75,14 +76,22 @@ impl Ord for Score {
     }
 }
 
+/// Result of searching a position.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SearchResult {
+    /// Best move in a position
+    ///
+    /// `None` only if there are no legal moves.
     pub best_move: Option<ChessMove>,
+    /// Position's quality for the player who is making a move.
     pub score: Score,
+    /// How many non-unique positions in the game tree were searched.
     pub nodes: u64,
+    /// `true` if search was unable to be fully completed for any reason.
     pub unfinished: bool,
 }
 
+/// Parallel search scheduler.
 #[derive(Debug)]
 pub struct ParallelSearch {
     #[allow(dead_code)]
@@ -97,6 +106,13 @@ pub struct ParallelSearch {
 }
 
 impl ParallelSearch {
+    /// Construct a new [`ParallelSearch`] with the specified amount of
+    /// worker threads and a given transposition table capacity.
+    ///
+    /// # Panics
+    /// - Panics if `thread_count` is zero.
+    /// - Panics if `tt_capacity` is zero.
+    /// - Panics if `tt_capacity * size_of::<Transposition>()` exceeds `isize::MAX`.
     pub fn new(thread_count: usize, tt_capacity: usize) -> Self {
         assert!(thread_count != 0, "Thread count must be at least one!");
 
@@ -129,15 +145,22 @@ impl ParallelSearch {
             tt,
         }
     }
+    /// Returns `true` if the parallel search currently running.
     pub fn is_searching(&self) -> bool {
         self.signal.load(Ordering::Relaxed) == SIGNAL_GO
     }
+    /// Returns how many search jobs are running/prepared to run.
     pub fn jobs_count(&self) -> usize {
         self.jobs_count
     }
+    /// Returns how many search jobs are not yet completed.
     pub fn pending_count(&self) -> usize {
         self.jobs_count() - self.results.len()
     }
+    /// Tells the waiter to wait until some search jobs are completed.
+    ///
+    /// # Panics
+    /// Panics if search is not currently running.
     pub fn add_to_waiter<'a>(
         &'a self,
         waiter: &mut Waiter<'a>,
@@ -149,6 +172,10 @@ impl ParallelSearch {
 
         waiter.add(&self.res_recv, deadline)
     }
+    /// Tries to collect the search results if all jobs are completed.
+    ///
+    /// # Panics
+    /// Panics if search is not currently running.
     pub fn try_collect(&mut self) -> Option<Vec<SearchResult>> {
         if !self.is_searching() {
             panic!("Search is paused.");
@@ -165,6 +192,10 @@ impl ParallelSearch {
         self.signal.store(SIGNAL_STOP, Ordering::SeqCst);
         Some(self.collect_results())
     }
+    /// Prepares a search job.
+    ///
+    /// # Panics
+    /// Panics if search is already running.
     pub fn prepare_search(
         &mut self,
         game: Game,
@@ -188,6 +219,10 @@ impl ParallelSearch {
         self.job_send.send(search_job).unwrap();
         index
     }
+    /// Starts to search the prepared search jobs.
+    ///
+    /// # Panics
+    /// Panics if search is alraedy running.
     pub fn go(&mut self) {
         if self.is_searching() {
             panic!("Search is already running.");
@@ -197,6 +232,11 @@ impl ParallelSearch {
         self.signal.store(SIGNAL_GO, Ordering::SeqCst);
         self.wakeup.wait();
     }
+    /// Forces all the jobs to immediately be completed even if
+    /// it means that they will not be fully searched.
+    ///
+    /// # Panics
+    /// Panics if search is not currently running.
     pub fn stop(&mut self) -> Vec<SearchResult> {
         if !self.is_searching() {
             panic!("Search is paused.");
@@ -209,6 +249,10 @@ impl ParallelSearch {
 
         self.collect_results()
     }
+    /// Same as [`ParallelSearch::stop`] but also terminates all the workers.
+    ///
+    /// # Panics
+    /// Panics if search is not currently running.
     pub fn quit(mut self) -> Vec<SearchResult> {
         if !self.is_searching() {
             panic!("Search is paused.");
@@ -221,6 +265,7 @@ impl ParallelSearch {
 
         self.collect_results()
     }
+    /// Clears the transposition table.
     pub fn clear_tt(&mut self) {
         self.tt.clear();
     }
